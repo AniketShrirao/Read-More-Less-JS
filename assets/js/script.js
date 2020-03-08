3a688e84a6b1aa61c6dabbf988687709f1896a2c
saniketprdxn <anikets@prdxn.com>
2020-03-08 12:11:48 +0000

    js completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,18 @@
 /* Author: Aniket*/
+// Select all read More Buttons
+var readMoreBtn = document.querySelectorAll(".More");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Assign TextToggle Function to Every Button
+readMoreBtn.forEach(function(btn){
+	btn.addEventListener('click',makeTextToggle);
+})
+// TextToggle function declaration
+function makeTextToggle() {	
+	if(this.innerText == "Read More") {
+		this.innerText = "Read Less";
+		this.previousElementSibling.children[0].classList = "readLess";
+	} else {
+		this.innerText = "Read More";
+		this.previousElementSibling.children[0].classList = "readMore";
+	}
+}
